b7388520caead0f24076670ed8b09e9d3be9d637
Katsumi Kumai <kuma.tennis.kttsm@gmail.com>
2016-09-16 17:28:39 +0000

    Finish ch9.2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,8 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       #success
       log_in user
-      params[:session][:remember] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_back_or user
     else
       #failure
       flash.now[:danger] = "Invalid email/password combination"
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -37,11 +37,28 @@ module SessionsHelper
     cookies.delete(:remember_token)
   end
   
+  #与えられたユーザーがログイン済みのユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
+  
+  
   # 現在のユーザーをログアウトする
   def log_out
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+  
+  #記憶したURL（もしくはデフォルト値）にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+  
+  #アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
   end
   
 end
